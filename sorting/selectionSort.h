9fb28eb14ede3015ce038940743153cf4ab4fd0b
UsePerson <cc22857312@gmail.com>
2021-01-22 15:49:03 +0800

    add selectionSort

---
+++ sorting/selectionSort.h
@@ -0,0 +1,20 @@
+#include "swap.h"
+
+void selectionSort(int list[], int arrSize){
+
+    int min;
+    for(int i = 0 ; i < (arrSize - 1) ; i ++){
+
+        min = i;
+        for(int j = i + 1 ; j < arrSize ; j ++){
+            if(list[ j ] < list[ min ]){
+                min = j;
+            }
+        }
+        swap(&list[i], &list[min]);
+    }
+}
+
+
+
+
